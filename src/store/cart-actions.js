0a266609df4e748ff82dd4970ba8f51f96e81f39
Ђорђе <37450617+Djolevil@users.noreply.github.com>
2021-09-27 16:25:45 +0200

    Finalize fetching logic and update cart-slice.js and App.js to prevent sending data to Firebase when application reloads

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -43,7 +43,10 @@ export const sendCartData = (cart) => {
       const sendRequest = async () => {
         const response = await fetch('https://redux-http-7a749-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { 
           method: 'PUT', 
-          body: JSON.stringify(cart), 
+          body: JSON.stringify({ 
+              items: cart.items, 
+              totalQuantity: cart.totalQuantity,
+            }), 
           }
         );
     
